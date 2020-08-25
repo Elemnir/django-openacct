80732dcd1628508591fcac5282186c828a03edff
Adam Howard <ahoward0920@gmail.com>
2020-08-25 13:52:52 -0400

    Prepping for an initial release

---
+++ src/openacct/__init__.py
@@ -0,0 +1,13 @@
+__version__     = "0.0.0"
+
+__title__       = "django-openacct"
+__description__ = "A pluggable Django app for managing research computing center project accounting."
+__uri__         = "https://github.com/Elemnir/django-openacct"
+
+__author__      = "Adam Howard"
+__email__       = "ahoward@utk.edu"
+
+__license__     = "BSD 3-clause"
+__copyright__   = "Copyright (c) 2020 Adam Howard"
+
+
