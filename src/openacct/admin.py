a67563d313b078d39eded2e80e80d61d439d4ce7
Adam Howard <ahoward0920@gmail.com>
2021-01-18 14:35:15 -0500

    More complete definition of the admin pages

--- src/openacct/admin.py
+++ src/openacct/admin.py
@@ -6,43 +6,74 @@ from .models    import (User, Project, UserProjectEvent, Account, System, Servic
 from .shortcuts import add_user_to_project, create_account
 
 
-class UserAdmin(admin.ModelAdmin):
-    pass
+class ToggleActiveAdminMixin(object):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if 'set_active' not in self.actions:
+            self.actions.append('set_active')
+        if 'set_inactive' not in self.actions:
+            self.actions.append('set_inactive')
+    
+    def set_active(self, request, queryset):
+        queryset.update(active=True)
+    set_active.short_description = 'Mark selected items as active'
+
+    def set_inactive(self, request, queryset):
+        queryset.update(active=False)
+    set_inactive.short_description = 'Mark selected items as inactive'
+
+
+class UserAdmin(ToggleActiveAdminMixin, admin.ModelAdmin):
+    list_display = ('name', 'realname', 'active', 'created')
 admin.site.register(User, UserAdmin)
 
 
-class ProjectAdmin(admin.ModelAdmin):
+class ProjectAdmin(ToggleActiveAdminMixin, admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         super().save_model(request, obj, form, change)
         if not change:
             add_user_to_project(obj.pi, obj)
             create_account(obj)
+    
+    class AccountInline(admin.TabularInline):
+        model = Account
+    
+    inlines = [AccountInline]
+    list_display = ('name', 'description', 'ldap_group', 'active', 'created')
 
 admin.site.register(Project, ProjectAdmin)
 
 
 class UserProjectEventAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('created', 'user', 'project', 'event_type')
 admin.site.register(UserProjectEvent, UserProjectEventAdmin)
 
 
-class AccountAdmin(admin.ModelAdmin):
-    pass
+class AccountAdmin(ToggleActiveAdminMixin, admin.ModelAdmin):
+    list_display = ('name', 'project', 'active', 'created', 'expires')
 admin.site.register(Account, AccountAdmin)
 
 
-class SystemAdmin(admin.ModelAdmin):
-    pass
+class SystemAdmin(ToggleActiveAdminMixin, admin.ModelAdmin):
+    class ServiceInline(admin.TabularInline):
+        model = Service
+
+    inlines = [ServiceInline]
+    list_display = ('name', 'description', 'active', 'created')
+
 admin.site.register(System, SystemAdmin)
 
 
-class ServiceAdmin(admin.ModelAdmin):
-    pass
+class ServiceAdmin(ToggleActiveAdminMixin, admin.ModelAdmin):
+    list_display = ('name', 'description', 'system', 'charge_rate', 'units', 'active', 'created')
 admin.site.register(Service, ServiceAdmin)
 
 
-class TransactionAdmin(admin.ModelAdmin):
-    pass
+class TransactionAdmin(ToggleActiveAdminMixin, admin.ModelAdmin):
+    list_display = (
+        'created', 'active', 'tx_type', 'service', 'account', 'creator', 
+        'amt_used', 'amt_charged'
+    )
 admin.site.register(Transaction, TransactionAdmin)
 
 
