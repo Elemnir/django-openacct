0c8cf2c4ca5f5f12219cde03b48099618dbc85ad
Adam Howard <ahoward0920@gmail.com>
2022-01-13 22:38:26 -0500

    Fixing the app names for contribs

--- src/openacct/contrib/login_records/apps.py
+++ src/openacct/contrib/login_records/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class LoginRecordsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "login_records"
+    name = "openacct.contrib.login_records"
--- src/openacct/contrib/token_auth/apps.py
+++ src/openacct/contrib/token_auth/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class TokenAuthConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "token_auth"
+    name = "openacct.contrib.token_auth"
