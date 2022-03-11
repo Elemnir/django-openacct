d040651bffaf2d318c29c148307d50b4c125d7ed
Adam Howard <ahoward0920@gmail.com>
2022-03-11 00:13:32 -0500

    Adding user friendly app display names

--- src/openacct/contrib/login_records/apps.py
+++ src/openacct/contrib/login_records/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class LoginRecordsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "openacct.contrib.login_records"
+    verbose_name = "Login History Records"
--- src/openacct/contrib/token_auth/apps.py
+++ src/openacct/contrib/token_auth/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class TokenAuthConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "openacct.contrib.token_auth"
+    verbose_name = "Token-Based Authentication"
