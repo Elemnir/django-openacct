67dd298f8b16c0f73ec1c42dc2630bd9fc8af843
Adam Howard <ahoward@utk.edu>
2023-09-18 14:13:41 -0400

    Adding login location information

--- src/openacct/contrib/login_records/models.py
+++ src/openacct/contrib/login_records/models.py
@@ -3,11 +3,24 @@ from django.db import models
 
 class LoginRecord(models.Model):
     when = models.DateTimeField()
-    host = models.CharField(max_length=64)
-    service = models.CharField(max_length=32)
-    method = models.CharField(max_length=32, blank=True, null=True)
+    host = models.CharField(max_length=256)
+    service = models.CharField(max_length=256)
+    method = models.CharField(max_length=256, blank=True, null=True)
     user = models.CharField(max_length=32)
     fromhost = models.CharField(max_length=256)
-
+    result = models.CharField(max_length=64, blank=True, default='success')
+    reason = models.CharField(max_length=256, blank=True, null=True)
+    locations = models.ManyToManyField("Location", through="LoginLocation")
+    
     def __str__(self):
         return f"{self.user} - {self.when}"
+
+class Location(models.Model):
+    country = models.CharField(max_length=256)
+    state = models.CharField(max_length=256)
+    city = models.CharField(max_length=256)
+
+class LoginLocation(models.Model):
+    role = models.CharField(max_length=32)
+    login = models.ForeignKey(LoginRecord, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
